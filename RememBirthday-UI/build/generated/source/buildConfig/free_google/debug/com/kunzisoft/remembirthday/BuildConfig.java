80671231830fa8f0a5845d76cb02996339614cdc
Nikolay <torbinnikolai@mail.ru>
2019-12-05 12:29:49 +0300

    First version of application

---
+++ RememBirthday-UI/build/generated/source/buildConfig/free_google/debug/com/kunzisoft/remembirthday/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kunzisoft.remembirthday;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kunzisoft.remembirthday.free";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "free_google";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.10-free";
+  // Fields from product flavor: free_google
+  public static final boolean FULL_VERSION = false;
+  public static final boolean GOOGLE_PLAY_VERSION = true;
+  // Fields from default config.
+  public static final String PRO_APPLICATION_ID = "com.kunzisoft.remembirthday.pro";
+}
